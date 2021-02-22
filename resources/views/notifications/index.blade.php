e745de5640e0d604f3b0f4720124acc31c88f3b2
Ken Kozma <kkozma@liquidfish.com>
2021-02-21 19:53:35 -0600

    Squashing more bugs

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -13,14 +13,16 @@ $carbon = new Carbon();
                             <h5 class="mb-1 font-weight-bold">{{ $notification->data['product']['name'] }}</h5>
                             <small>{{$carbon->parse($notification->created_at)->diffForHumans()}}</small>
                         </div>
+                        <p class="mb-1 font-weight-bold">Current Quantity: {{ $notification->data['product']['quantity'] }}</p>
+                        <p class="mb-1 font-weight-bold">Low Quantity: {{ $notification->data['product']['notification_quantity'] }}</p>
                         <p class="mb-1">
                             {{ $notification->data['details'] }}
                         </p>
                         <a class="text-white btn btn-primary mr-2" href="/admin/transactions/create/{{$notification->data['product']['id']}}">
                             Increase Quantity
                         </a>
-                        <a class="text-white btn btn-info mr-2" href="">Mark as Read</a>
-                        <a class="text-white btn btn-danger mr-2" href="">Delete</a>
+{{--                        <a class="text-white btn btn-info mr-2" href="">Mark as Read</a>--}}
+{{--                        <a class="text-white btn btn-danger mr-2" href="">Delete</a>--}}
                     </div>
                 @endforeach
             @else
